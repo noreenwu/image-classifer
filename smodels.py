74e29d65aa3b3434727adb678bf1547756b0229f
Noreen Wu <noreenwu@gmail.com>
2020-09-08 20:45:24 -0400

    fix: improper resize sequence led to tensor mismatch

--- smodels.py
+++ smodels.py
@@ -13,7 +13,8 @@ def load_train_data(root_path, batch_size):
                                   std=[0.229, 0.224, 0.225]),
              ])
     test_transforms = transforms.Compose(
-            [transforms.Resize(224),
+            [transforms.Resize(255),
+             transforms.CenterCrop(224),
              transforms.ToTensor(),
              transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225]),
