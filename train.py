c30c7898b38d9d71878a2f8da538d1117db5333e
Noreen Wu <noreenwu@gmail.com>
2020-09-10 21:49:31 -0400

    clean/refactor

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse as ap
 import os
 import torch
 from torchvision import datasets, transforms, models
@@ -9,49 +9,86 @@ from collections import OrderedDict
 from smodels import load_train_data, create_classifier, train
 
 
-parser = argparse.ArgumentParser(
-  description='Train a specified architecture on specified data'
-)
-parser.add_argument("train_data", help="training data directory")
+def define_args():    
+    parser = ap.ArgumentParser(
+      description='Train a specified architecture on specified data'
+    )
+    parser.add_argument("train_data_dir", help="training data directory")
 
-parser.add_argument("-a", "--arch", default="vgg16", help="pretrained architecture")
-parser.add_argument("-s", "--save_dir", default="checkpoints", help="checkpoint save directory")
-parser.add_argument("-l", "--learning_rate", type=float, default=0.001, help="learning rate")
-parser.add_argument("-hu", "--hidden_units", type=int, default=256, help="hidden_units")
-parser.add_argument("-e", "--epochs", type=int, default=1, help="increase output verbosity")
-parser.add_argument("-g", "--gpu", action="store_true", help="use GPU")
-
-args = parser.parse_args()
-
-train_dir = args.train_data
-train_dir_exists = os.path.isdir(train_dir)
-
-if not train_dir_exists:
-  print("Could not find training data directory, {}".format(train_dir))
-  exit(1)
-else:
-  print("Training directory {}: ok".format(train_dir))
-
-checkpoints_dir = args.save_dir
-checkpoints_dir_exists = os.path.isdir(checkpoints_dir)
-if not checkpoints_dir_exists:
-  print("Could not find checkpoints directory, {}".format(checkpoints_dir))
-  exit(1)
-print("Checkpoints directory {}: ok".format(checkpoints_dir))
-
-print("Will train with {} architecure".format(args.arch))
-print("Learning rate is {}".format(args.learning_rate))
-print("Number of hidden units: {}".format(args.hidden_units))
-print("Epochs: {}".format(args.epochs))
-
-if args.gpu:
-  print("Use GPU")
-else:
-  print("Use CPU")
+    parser.add_argument("-a", "--arch", default="vgg16", help="pretrained architecture")
+    parser.add_argument("-s", "--save_dir", default="checkpoints", help="checkpoint save directory")
+    parser.add_argument("-l", "--learning_rate", type=float, default=0.001, help="learning rate")
+    parser.add_argument("-hu", "--hidden_units", type=int, default=256, help="hidden_units")
+    parser.add_argument("-e", "--epochs", type=int, default=1, help="increase output verbosity")
+    parser.add_argument("-g", "--gpu", action="store_true", help="use GPU")
+    
+    return parser
 
 
-###
-trainloader, testloader, class_to_idx = load_train_data("flowers", 32) 
+def get_options():
+    parser = define_args()
+    args = parser.parse_args()    
+
+    train_dir = args.train_data_dir
+
+    if not os.path.isdir(train_dir):
+      print("Could not find training data directory, {}".format(train_dir))
+      exit(1)
+    else:
+      print("Training data directory: {}".format(train_dir))
+
+    checkpoints_dir = args.save_dir
+    checkpoints_dir_exists = os.path.isdir(checkpoints_dir)
+    if not checkpoints_dir_exists:
+      print("Could not find checkpoints directory: {}".format(checkpoints_dir))
+      exit(1)
+    print("Save checkpoint to directory: {}".format(checkpoints_dir))
+
+    print("Train with architecure: {}".format(args.arch))
+    print("Learning rate: {}".format(args.learning_rate))
+    print("Number of hidden units: {}".format(args.hidden_units))
+    print("Epochs: {}".format(args.epochs))    
+    
+    if args.gpu:
+        specified_device = "cuda"
+    else:
+        specified_device = "cpu"
+
+
+    if specified_device == "cuda":
+        if not torch.cuda.is_available():
+            print("GPU specified but not available. Sorry")
+            exit(1)
+
+    print("Device: {}".format(specified_device))
+    
+    return train_dir, checkpoints_dir, args.arch, args.learning_rate, args.hidden_units, args.epochs, specified_device
+
+
+def main(raw_args=None):
+    
+    train_dir, checkpoints_dir, arch, learning_rate, hidden_units, epochs, specified_device = get_options()
+    
+    
+    # set device to cuda, if specified
+    if specified_device == "cuda":
+        device = torch.device("cuda")
+    
+    
+    # load training and test data; get the class_to_idx mapping
+    trainloader, testloader, class_to_idx = load_train_data(train_dir, 32) 
+    
+    print(class_to_idx)
+    exit()
+    
+    
+if __name__ == '__main__':
+    main()
+    
+
+    
+
+    
 
 print(class_to_idx)
 
@@ -60,25 +97,9 @@ model = models.vgg16(pretrained=True)           ## default; offer other choices
 
 classifier = create_classifier()
 
-# classifier = nn.Sequential(OrderedDict([
-#     ('fc1', nn.Linear (25088, 1024)),
-#     ('relu', nn.ReLU ()),
-#     ('dropout', nn.Dropout (p = 0.2)),  # down from 0.3    
-#     ('fc2', nn.Linear (1024, 102)),     # swap dropout and fc2 
-#     ('output', nn.LogSoftmax (dim =1))
-# ]))
-
 model.classifier = classifier
 
 print(model)
-
-# exit()
-# for param in model.parameters():
-#    param.requires_grad = False
-
-# for name, params in model.named_children():
-#     print(name)
-    
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -91,54 +112,6 @@ optimizer= optim.Adam(model.classifier.parameters(), lr=0.001)    ## learning ra
     
 train(model, device, trainloader, testloader, optimizer, criterion, args.epochs)
     
-# epochs = args.epochs
-# steps = 0
-# running_loss = 0
-# print_every = 5
-# model.to(device)
-
-# for epoch in range(epochs):
-#     for images, labels in trainloader:
-#         steps += 1
-
-#         images, labels = images.to(device), labels.to(device)
-
-#         optimizer.zero_grad()
-
-#         logps = model(images)
-#         loss = criterion(logps, labels)
-#         loss.backward()
-#         optimizer.step()
-
-#         running_loss += loss.item()   
-
-#         if steps % print_every == 0:
-#             model.eval()
-#             test_loss = 0
-#             accuracy = 0 
-
-#             for images, labels in testloader:
-#                 images, labels = images.to(device), labels.to(device)
-
-#                 logps = model(images)
-#                 loss = criterion(logps, labels)
-#                 test_loss += loss.item()         
-
-#                 # calculate accuracy
-#                 ps = torch.exp(logps)
-#                 top_ps, top_class = ps.topk(1, dim=1)
-#                 equality = top_class == labels.view(*top_class.shape)
-#                 accuracy += torch.mean(equality.type(torch.FloatTensor)).item()                 
-
-#                 print("Epoch: {}/{}.. ".format(epoch+1, epochs),
-#                       "Training Loss: {:.3f}.. ".format(running_loss/print_every),
-#                       "Test Loss: {:.3f}.. ".format(test_loss/len(testloader)),
-#                       "Test Accuracy: {:.3f}".format(accuracy/len(testloader)))
-
-#                 running_loss = 0
-#                 model.train()
-
-                
 
 model.class_to_idx = class_to_idx    # saved above
 
@@ -154,3 +127,5 @@ torch.save(checkpoint, 'checkpoint.pth')
 
 
 print("end")
+
+
